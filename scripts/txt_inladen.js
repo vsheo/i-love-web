af84cf4b20cb74b1abae3f76aeda8ab211717651
vsheo <vireshsheoratan@gmail.com>
2024-10-16 21:54:48 +0200

    new html for journal

--- scripts/txt_inladen.js
+++ scripts/txt_inladen.js
@@ -1,16 +1,8 @@
 
-fetch('../txt_bestanden/day_journal/sprint_1/8_9_24.txt') // Het pad naar je tekstbestand
-      .then(response => {
-        if (!response.ok) { // Controleer of het verzoek succesvol was
-          throw new Error('Netwerkprobleem: ' + response.statusText);
-        }
-        return response.text(); // Zet de response om in tekst
-      })
-      .then(data => {
-        document.getElementById('text-container').textContent = data; // Plaats de opgehaalde tekst in de div
-      })
-      .catch(error => console.error('Fout bij het laden van het bestand:', error));
+let articleScroll = document.querySelector('article')
 
+articleLink.addEventListener('scroll', scrollAnimation)
 
-
-// werkt alleen als ik vscode loive server aan heb staan
+function scrollAnimation(event) {
+  event.target.classList.toggle('alternatecolor')
+}
