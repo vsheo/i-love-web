eaf6948218b7b16dc5b0182a9f306b46acabb72c
vsheo <vireshsheoratan@gmail.com>
2025-02-22 12:24:43 +0100

    file structuur voor liquid

---
+++ server.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import { Liquid } from 'liquidjs';
+
+const squadResponse = await fetch('https://fdnd.directus.app/items/squad?filter={"_and":[{"cohort":"2425"},{"tribe":{"name":"FDND Jaar 1"}}]}')
+const squadResponseJSON = await squadResponse.json()
+const app = express()
+
+app.use(express.static('public'))
+const engine = new Liquid();
+app.engine('liquid', engine.express()); 
+app.set('views', './views')
+app.use(express.urlencoded({extended: true}))
+
+
+app.get('/', async function (request, response) {
+  const personResponse = await fetch('https://fdnd.directus.app/items/person/?fields=website,squads.squad_id.name&filter={"squads":{"squad_id":{"name":{"_eq":"1G"}}}}')
+  const personResponseJSON = await personResponse.json()
+
+  response.render('head.liquid', {website: personResponseJSON.data})
+})
+
+app.post('/', async function (request, response) {
+  response.redirect(303, '/')
+})
+
+app.set('port', process.env.PORT || 8080)
+
+app.listen(app.get('port'), function () {
+  console.log(`Application started on http://localhost:${app.get('port')}`)
+})
