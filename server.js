021cd71edff9f1ffe401c12e6e183da1c96891a2
vsheo <vireshsheoratan@gmail.com>
2025-02-23 22:58:53 +0100

    data van json naar html

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import { Liquid } from 'liquidjs';
 
-const squadResponse = await fetch('https://fdnd.directus.app/items/squad?filter={"_and":[{"cohort":"2425"},{"tribe":{"name":"FDND Jaar 1"}}]}')
-const squadResponseJSON = await squadResponse.json()
+import fs from 'fs';
+import path from 'path';
+
 const app = express()
 
 app.use(express.static('public'))
@@ -12,12 +13,31 @@ app.set('views', './views')
 app.use(express.urlencoded({extended: true}))
 
 
-app.get('/', async function (request, response) {
-  const personResponse = await fetch('https://fdnd.directus.app/items/person/?fields=website,squads.squad_id.name&filter={"squads":{"squad_id":{"name":{"_eq":"1G"}}}}')
-  const personResponseJSON = await personResponse.json()
+// app.get('/', async function (request, response) {
+//   const personResponse = await fetch('/database/sprint1.json')
+//   const personResponseJSON = await personResponse.json()
 
-  response.render('index.liquid', {website: personResponseJSON.data})
-})
+//   response.render('index.liquid', {website: personResponseJSON.data})
+// })
+
+app.get('/', async function (request, response) {
+  try {
+    // Pad naar het JSON-bestand in je project
+    const jsonPath = './database/sprint1.json';
+    
+    // Lees het bestand synchronisch met fs
+    const data = fs.readFileSync(jsonPath, 'utf8'); 
+
+    // Parse de inhoud van het bestand als JSON
+    const sprintDataJSON = JSON.parse(data);
+
+    // Render de Liquid template met de data
+    response.render('index.liquid', { data: sprintDataJSON.articles });
+  } catch (err) {
+    console.error('Error reading the JSON file:', err);
+    response.status(500).send('Error reading JSON file');
+  }
+});
 
 app.post('/', async function (request, response) {
   response.redirect(303, '/')
