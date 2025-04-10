03df01ad78101293e1fa57e2be7e80524b1f2018
vsheo <vireshsheoratan@gmail.com>
2025-04-10 16:40:36 +0200

    scripts naar main.js

--- public/scripts/hamburger.js
+++
@@ -1,8 +0,0 @@
-
-const openMenu = document.querySelector(".open-menu");
-const menu = document.querySelector("nav");
-
-
-openMenu.addEventListener("click", () => {
-    menu.classList.toggle("hidden");
-});
